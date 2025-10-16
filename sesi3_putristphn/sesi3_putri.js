3a13694d065c93d4ebd813802a6649b779d4d784
Putri Stephanie Lesilolo <putri.stephanie16@gmail.com>
2025-10-16 16:14:01 +0700

    Add screenshots for sesi3 and sesi4 tasks and update readme

--- sesi3_putristphn/sesi3_putri.js
+++ sesi3_putristphn/sesi3_putri.js
@@ -1,12 +1,12 @@
-// Buat segitiga menggunakan loop
-let tinggi = 4; // jumlah baris segitiga
+// Create a triangle using a loop
+let tinggi = 4; // number of triangle rows
 let hasil = "";
 
 for (let i = 1; i <= tinggi; i++) {
   for (let j = 1; j <= i; j++) {
     hasil += "*";
   }
-  hasil += "\n"; // pindah baris
+  hasil += "\n"; // move to the next line
 }
 
 console.log(hasil);
